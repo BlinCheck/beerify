22ded4bd761d92f2556e72d1733b379114b68d7f
ashmrkvch <61711508+ashmrkvch@users.noreply.github.com>
2020-05-26 16:56:19 +0300

    Controllers

---
+++ backend/Controllers/IPlaceController.cs
@@ -0,0 +1,22 @@
+﻿using Beerify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class IPlaceController : ApiController
+    {
+        [HttpPost]
+        public Place CreatePlace(int placeId, string name, string location,
+            string picturePath, string description)
+        {
+            return new Place(placeId, name, location,
+            picturePath, description);
+
+        }
+    }
+}
---
+++ backend/Controllers/IUserController.cs
@@ -0,0 +1,28 @@
+﻿using Beerify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[usercontroller]")]
+    public class IUserController : ApiController
+    {
+        [HttpPost]
+        public User CreateUser(int userId, string email, string password, string name,
+            DateTime birthDate, string picturePath, int telegramId, string bio)
+        {
+            return new User(userId, email, password, name,
+            birthDate, picturePath, telegramId, bio);
+        }
+
+        [HttpPost]
+        public List<UserSub> ShowUserSub(int UserID)
+        {
+            return UserSub.DownLoadAllForUser(UserID);
+        }
+    }
+}
